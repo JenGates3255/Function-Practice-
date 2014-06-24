8de2f28a19089382c44a93983e391109725811cc
JenGates3255 <Jengates3255@gmail.com>
2014-06-24 16:26:15 -0600

    First commit

---
+++ main.js
@@ -0,0 +1,36 @@
+// var triplefive = function(){
+// 	for (var i = 0; i < 3; i++ ){
+// 		console.log("five!");
+// 	}
+// }
+// triplefive()
+
+// var lastletter = function(string){
+// 	return string[string.length-1] 
+// }
+// lastletter("Jen")
+
+// var square = function(number) {
+// 	return number*number;
+// }
+// square (5)
+
+// var negate = function(negative){
+// 	return -negative;
+// }
+// negate(5)
+// //*********************************
+// var toArray = function(numbers){
+// 	return numbers;
+// }
+// toArray(2)
+//*********************************
+var startsWithA = function(word){
+	return word.charAt(0) === 'a'
+}
+startsWithA("apple")
+
+var excite = function(fun){
+	return fun+"!!!";
+}
+excite("go")
